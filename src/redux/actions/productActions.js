7507bb416a05bb32c1e4e983eb9df8d65776de09
jaeyoung99-lee <leejy0715@naver.com>
2024-09-02 17:05:35 +0900

    redux toolkit 사용하여 기존의 'hnm-redux' 프로젝트를 업데이트

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,10 +1,13 @@
+import { productActions } from "../reducers/productReducer";
+
 function getProducts(searchQuery) {
   return async (dispatch, getState) => {
     let url = `https://my-json-server.typicode.com/jaeyoung99-lee/CodingSister_hnm-shopping-mall/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log("data :", data);
-    dispatch({ type: "GET_PRODUCT_SUCCESS", payload: { data } });
+    // dispatch({ type: "GET_PRODUCT_SUCCESS", payload: { data } });
+    dispatch(productActions.getAllProducts({ data }));
   };
 }
 
@@ -14,7 +17,8 @@ function getProductDetail(id) {
     let response = await fetch(url);
     let data = await response.json();
     console.log("data :", data);
-    dispatch({ type: "GET_SINGLE_PRODUCT_SUCCESS", payload: { data } });
+    // dispatch({ type: "GET_SINGLE_PRODUCT_SUCCESS", payload: { data } });
+    dispatch(productActions.getSingleProduct({ data }));
   };
 }
 export const productAction = { getProducts, getProductDetail };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,28 @@
-import { applyMiddleware, createStore } from "redux";
-import { thunk } from "redux-thunk";
-import rootReducer from "./reducers/index";
+// import { applyMiddleware, createStore } from "redux";
+// import { thunk } from "redux-thunk";
+// import rootReducer from "./reducers/index";
 // import rootReducer from "./reducers"; // index.js 파일이 기본 파일이라 폴더명만 써도 index.js 파일을 읽어옴
-import { composeWithDevTools } from "@redux-devtools/extension";
+// import { composeWithDevTools } from "@redux-devtools/extension";
+import { configureStore } from "@reduxjs/toolkit";
+import authenticateReducer from "./reducers/authenticateReducer";
+import productReducer from "./reducers/productReducer";
 
-let store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+// createStore를 사용하기 위해 필요한 것들
+// 1. combinereducer
+// 2. thunk
+// 3. applyMiddleware
+// 4. composeWithDevTools
+
+// let store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+const store = configureStore({
+  reducer: {
+    auth: authenticateReducer,
+    product: productReducer,
+  },
+});
 
 export default store;
